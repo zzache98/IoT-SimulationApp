28fa8c0121989397d97c7285ec05f26a176b9f33
Zacharias Lönnqvist <zzache98@gmail.com>
2022-10-16 23:42:01 +0200

    final final

--- SQLQuery1.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE DeviceInfo (
-	DeviceId nvarchar(450) not null primary key,
-	ConnectionString nvarchar(max) null,
-	DeviceName nvarchar(max) null,
-	DeviceType nvarchar(max) null,
-	Location nvarchar(max) null,
-	Owner nvarchar(max) null
-)
